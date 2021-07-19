781db8f3cbe23a838dc82cc859194e61563fd933
Neil Leonard <quantumchamploo@gmail.com>
2021-07-18 18:19:05 -0700

    adding neat lib

---
+++ workSpace/initTest.py
@@ -0,0 +1,38 @@
+import neat
+import os
+
+print("first test")
+
+def run(config_file):
+    """
+    runs the NEAT algorithm to train a neural network to play flappy bird.
+    :param config_file: location of config file
+    :return: None
+    """
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+    p = neat.Population(config)
+
+    genomes = p.population.items()
+    genome1key = next(iter(p.population))
+    genome = p.population[genome1key]
+    print(genome1key)
+    print(genome)
+
+    net = neat.nn.FeedForwardNetwork.create(genome, config)
+    # print(genomes)
+    # for genome_id, genome in genomes:
+    #     print(genome_id)
+    #     print(genome)
+    #     print("\n \n \n")
+
+
+if __name__ == '__main__':
+    #print('in start')
+    # Determine path to configuration file. This path manipulation is
+    # here so that the script will run successfully regardless of the
+    # current working directory.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.txt')
+    run(config_path)
